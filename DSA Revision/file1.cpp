cd214a52aa3d9a0e7be013c47d0e649677ee10b9
TheOathkeeper <95406141+TheOathkeeper@users.noreply.github.com>
2022-02-19 14:20:14 +0530

    DSA Revision Class - 19 Feb 2022

---
+++ DSA Revision/file1.cpp
@@ -0,0 +1,18 @@
+// Algorithms refer to the pseudo code that talks about the logic and stuff.
+// Algorithms are written by domain experts.
+// Algorithms can be specified into 3 different ways: Natural language, flow charts, pseudo code.
+// Recursion is an application of stacks data structure.
+// Types of recursion: direct and indirect.
+
+// Example of direct recursion:
+int func(int n){
+    if (n==0)
+        return 1;
+    else
+        return (func(n-1));   // Function is called directly
+}
+
+// Example of indirect recursion:
+int func2(int n){
+    
+}
---
+++ DSA Revision/file2.cpp
@@ -0,0 +1,2 @@
+// formula to calculate base address: (given base address) + (byte size)*(index value - lower count)
+// length of an array: upper bound - lower bound + 1
