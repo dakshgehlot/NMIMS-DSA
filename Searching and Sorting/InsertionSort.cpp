e5a2a9f2b9411f4f93b5a4b644937f3ffd08a50e
TheOathkeeper <dakshgehlot30@gmail.com>
2022-04-21 11:35:55 +0530

    new codes

--- Searching and Sorting/InsertionSort.cpp
+++ Searching and Sorting/InsertionSort.cpp
@@ -2,5 +2,28 @@
 using namespace std;
 
 int main(){
-      
-}
+    int n, x, i, temp;
+    cout << "\nHow many input items are there? ";
+    cin >> n;
+    int a[n];
+    
+    cout << endl;
+    for(i=0;i<n;i++){
+        cout << "Enter element " << i+1 <<": ";
+        cin >> a[i];}
+
+    cout << "\nThe input array is: ";
+    for(i=0;i<n;i++){cout << a[i] << " ";}
+
+
+    cout << "\nThe sorted array is: ";
+    for (i=1; i<=n; i++){
+        if (a[i] <  a[i-1]){
+            temp = a[i];
+            a[i] = a[i-1];
+            a[i-1] = temp; 
+        }
+        cout << a[i-1] << " ";
+    }
+    return 0;
+} 
