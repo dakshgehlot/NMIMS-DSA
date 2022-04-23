6562f63b662d2617d6cce6d0495caf8dcd210882
TheOathkeeper <dakshgehlot30@gmail.com>
2022-04-24 00:03:36 +0530

    New Codes

---
+++ Time-And-Space-Complexity-A8/Task3-a2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, f0 = 0, f1 = 1, fi = 0;
+    cout << "\nEnter the number of terms: ";
+    cin >> num;
+
+    for (int i=0;i<num;i++){
+        cout << f0 << " ";
+        fi = f0 + f1;
+        f0 = f1;
+        f1 = fi;
+    }
+    return 0;
+}
+
+// Space complexity: 5
+// Time complexity: O(n)
